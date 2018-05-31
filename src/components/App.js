6e7758f76f6ac6bc24fe53e172b045ada44a4804
Oasiris <teracontinens@gmail.com>
2018-05-31 00:24:33 -0700

    Started working on mock UI components

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,14 @@ import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './../styles/App.css';
 
+import Material from './Material.js';
+
+
 class App extends Component {
   render() {
     return (
       <div className="App">
+      
         <header className="App-header">
           <h1 className="App-title">Breath of the Wild Cookbook</h1>
           {/* <img src={logo} className="App-logo" alt="logo" /> */}
@@ -14,6 +18,11 @@ class App extends Component {
         {/* <p className="App-intro"> */}
           {/* To get started, edit <code>src/App.js</code> and save to reload. */}
         {/* </p> */}
+
+        <div className="container">
+          <Material />        
+        </div>
+        
       </div>
     );
   }
---
+++ src/components/Material.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import '../styles/Material.css'
+
+
+
+export default class Material extends Component {
+  render() {
+    return <div className="mat-card">
+      <div className="mat-icon">
+
+      </div>
+      <div className="mat-name">
+        Hello
+      </div>
+    </div>
+  }
+}
