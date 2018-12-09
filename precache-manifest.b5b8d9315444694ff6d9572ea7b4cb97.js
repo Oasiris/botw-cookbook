4ed0b7e95a13a876653eec9ba9f09d42366367f4
Oasiris <teracontinens@gmail.com>
2018-12-09 01:48:48 -0500

    First 'deploy'

---
+++ precache-manifest.b5b8d9315444694ff6d9572ea7b4cb97.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "9e67238674a0d6b49d2a3481516f75bf",
+    "url": "/static/media/fontello.9e672386.ttf"
+  },
+  {
+    "revision": "956249212adfd6a99e1775b299338583",
+    "url": "/static/media/fontello.95624921.eot"
+  },
+  {
+    "revision": "4a9840f882f0bc33d18b5d239808cd7d",
+    "url": "/static/media/fontello.4a9840f8.svg"
+  },
+  {
+    "revision": "2338f380f2f3f2c9d4964fad0b527c15",
+    "url": "/static/media/fontello.2338f380.woff2"
+  },
+  {
+    "revision": "0ff0318f2d67a29684bda965aa78ee67",
+    "url": "/static/media/fontello.0ff0318f.woff"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0e46cae0f36a712eb358",
+    "url": "/static/js/main.0e46cae0.chunk.js"
+  },
+  {
+    "revision": "d5b2888707e7cc4e3d9d",
+    "url": "/static/js/1.d5b28887.chunk.js"
+  },
+  {
+    "revision": "0e46cae0f36a712eb358",
+    "url": "/static/css/main.0e8329e2.chunk.css"
+  },
+  {
+    "revision": "fdbaebb1914609cef8b28eefd5a49837",
+    "url": "/index.html"
+  }
+];
